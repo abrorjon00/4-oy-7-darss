1cb6acaf7189c1ec272745ef473b5c7723a91a99
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-10-05 06:15:17 +0300

    dars

---
+++ app.js/app.js
@@ -0,0 +1,107 @@
+// Math ga misollar
+
+// 1-misol
+
+// let randomSon = Math.trunc(Math.random() * 100);
+// console.log(randomSon);
+
+// 3-misol
+
+// let sonlar = [45, 12, 78, 3, 50];
+// console.log(Math.max(...sonlar));
+// console.log(Math.min(...sonlar));
+
+// 4-misol
+
+// let son = 4.6;
+// console.log(Math.ceil(son)); 
+// console.log(Math.floor(son));
+
+// Tenary opetaoriga misollar
+
+// 1-misol
+
+// let son = -10;
+// console.log(son >= 0 ? "Musbat son" : "Manfiy son");
+
+// 2-misol
+
+// let yosh = 20;
+// console.log(yosh >= 18 ? "Ruxsat etilgan" : "Ruxsat etilmagan");
+
+// 3-misol
+
+// let son = 7;
+// console.log(son % 2 === 0 ? "Juft son" : "Toq son");
+
+// for sikl operatoriga misollar
+
+// 1-misol
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// 3-misol
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i >= 30 && i <= 50) {
+//         console.log(i);
+//     }
+// }
+
+// 4-misol
+
+// let sonlar = [-2, 4, -1, 3, 0, 10, -7, 5, -6, 8, 2, -9, 0, 12, -15];
+// let musbat = 0, manfiy = 0;
+// for (let i = 0; i < 15; i++) {
+//     if (sonlar[i] >= 0) {
+//         musbat++;
+//     } else {
+//         manfiy++;
+//     }
+// }
+// console.log("Musbat sonlar: " + musbat);
+// console.log("Manfiy sonlar: " + manfiy);
+
+// 6-misol
+// let yigindi = 0;
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 !== 0) {
+//         yigindi += i;
+//     }
+// }
+// console.log(yigindi);
+
+
+// 7-misol
+
+// let yoshlar = [15, 22, 18, 12, 19];
+// for (let i = 0; i < 5; i++) {
+//     if (yoshlar[i] >= 18) {
+//         console.log("Voyaga yetgan");
+//     } else {
+//         console.log("Voyaga yetmagan");
+//     }
+// }
+
+// 8-misol
+
+// let juft = 0;
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         juft++;
+//     }
+// }
+// console.log(juft);
+
+// 12-misol
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
